748a790f9d9fc1af02d456ee7672fd0cbeeeadec
spline <dmcemin@gmail.com>
2026-05-22 19:09:20 +0200

    add: ApiV1Prefix

--- app/core/config.py
+++ app/core/config.py
@@ -9,8 +9,13 @@ class RunConfig(BaseModel):
     port: int = 8000
 
 
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+
+
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
 
 
 class DatabaseConfig(BaseModel):
