4a7b06732ab730d69d62b62dbcfbac9c9a50c461
spline <dmcemin@gmail.com>
2026-06-20 17:31:05 +0200

    use: computed_field for db url

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 from enum import StrEnum
 from enum import auto
 
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field, SecretStr
 from pydantic import HttpUrl
 from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
@@ -37,7 +37,12 @@ class TestAPIConfig(BaseModel):
 
 
 class DatabaseConfig(BaseModel):
-    url: PostgresDsn
+    host: str
+    port: int = 5432
+    name: str
+    user: str
+    password: SecretStr
+
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 50
@@ -50,6 +55,12 @@ class DatabaseConfig(BaseModel):
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
         "pk": "pk_%(table_name)s",
     }
+
+    @computed_field
+    def url(self) -> PostgresDsn:
+        return PostgresDsn(
+            f"postgresql+asyncpg://{self.user}:{self.password.get_secret_value()}@{self.host}:{self.port}/{self.name}"
+        )
 
 
 class Settings(BaseSettings):
