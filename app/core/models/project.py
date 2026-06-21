2ecb3b7b696c0b0f5c1525a6a9c7185c6f9b1d2c
spline <dmcemin@gmail.com>
2026-06-21 19:49:46 +0200

    add: Project model

---
+++ app/core/models/project.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import Text
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+from app.constants import ProjectLimits
+from app.core.models.base import Base
+from app.core.models.mixins import IntIdPkMixin
+from app.core.models.mixins import ObservableMixin
+
+if TYPE_CHECKING:
+    from app.core.models.user import User
+
+
+class Project(IntIdPkMixin, ObservableMixin, Base):
+    name: Mapped[str] = mapped_column(String(ProjectLimits.NAME_MAX))
+    description: Mapped[str] = mapped_column(Text)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+
+    user: Mapped[User] = relationship(back_populates="projects")
--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,13 +1,25 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 from app.constants.user import UserLimits
 from app.core.models.base import Base
 from app.core.models.mixins import ObservableMixin
 from app.core.models.mixins.int_id_pk import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from app.core.models.project import Project
+
 
 class User(IntIdPkMixin, ObservableMixin, Base):
     username: Mapped[str] = mapped_column(String(UserLimits.USERNAME_MAX), unique=True)
     hashed_password: Mapped[str] = mapped_column(String(UserLimits.HASHED_PASSWORD_MAX))
+
+    projects: Mapped[list[Project]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
