39602ef19c790f235a6748a3a0aff73280386e99
spline <dmcemin@gmail.com>
2026-06-21 17:59:44 +0200

    add: User model

---
+++ app/core/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from app.constants.user import UserLimits
+from app.core.models.base import Base
+from app.core.models.mixins.int_id_pk import IntIdPkMixin
+
+
+class User(IntIdPkMixin, Base):
+    username: Mapped[str] = mapped_column(String(UserLimits.USERNAME_MAX), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(UserLimits.HASHED_PASSWORD_MAX))
