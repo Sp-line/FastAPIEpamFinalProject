98f860100e807fc081037b6e88bb9343edc2b0eb
spline <dmcemin@gmail.com>
2026-06-21 19:04:53 +0200

    add: ObservableMixin for User model

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -4,9 +4,10 @@ from sqlalchemy.orm import mapped_column
 
 from app.constants.user import UserLimits
 from app.core.models.base import Base
+from app.core.models.mixins import ObservableMixin
 from app.core.models.mixins.int_id_pk import IntIdPkMixin
 
 
-class User(IntIdPkMixin, Base):
+class User(IntIdPkMixin, ObservableMixin, Base):
     username: Mapped[str] = mapped_column(String(UserLimits.USERNAME_MAX), unique=True)
     hashed_password: Mapped[str] = mapped_column(String(UserLimits.HASHED_PASSWORD_MAX))
