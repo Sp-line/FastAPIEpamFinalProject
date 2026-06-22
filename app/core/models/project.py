6a18e65f88a9bc1d851abba207dc25593de812dd
spline <dmcemin@gmail.com>
2026-06-22 13:52:45 +0200

    add: Check Constraints for Project model

--- app/core/models/project.py
+++ app/core/models/project.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from sqlalchemy import CheckConstraint
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy import Text
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
@@ -41,4 +43,11 @@ class Project(IntIdPkMixin, ObservableMixin, Base):
     user_associations: Mapped[list[ProjectMemberAssociation]] = relationship(
         back_populates="project",
         cascade="all, delete-orphan",
+    )
+
+    __table_args__ = (
+        CheckConstraint(
+            func.char_length(name) >= ProjectLimits.NAME_MIN,
+            name="check_project_name_min_len",
+        ),
     )
