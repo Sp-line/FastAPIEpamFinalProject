534c50035166e0bff7a8fca809c9ee1a0f2a9584
spline <dmcemin@gmail.com>
2026-06-23 14:47:25 +0200

    add: ServiceProvider & RepositoryProvider imports to __all__

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -1,3 +1,5 @@
-__all__ = ("InfrastructureProvider",)
+__all__ = ("InfrastructureProvider", "RepositoryProvider", "ServiceProvider")
 
 from app.dependencies.infrastructure import InfrastructureProvider
+from app.dependencies.repositories import RepositoryProvider
+from app.dependencies.services import ServiceProvider
