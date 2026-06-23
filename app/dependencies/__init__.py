3a84238e112173223d7f706793169f3108729637
spline <dmcemin@gmail.com>
2026-06-23 16:21:49 +0200

    add: UsagesProvider to __all__

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -1,5 +1,11 @@
-__all__ = ("InfrastructureProvider", "RepositoryProvider", "ServiceProvider")
+__all__ = (
+    "InfrastructureProvider",
+    "RepositoryProvider",
+    "ServiceProvider",
+    "UsagesProvider",
+)
 
 from app.dependencies.infrastructure import InfrastructureProvider
 from app.dependencies.repositories import RepositoryProvider
 from app.dependencies.services import ServiceProvider
+from app.dependencies.usages import UsagesProvider
