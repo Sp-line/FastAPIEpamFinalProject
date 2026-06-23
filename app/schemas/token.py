a91be374546f291134270f03db8d1156a86a7aa4
spline <dmcemin@gmail.com>
2026-06-23 16:10:21 +0200

    add: JWTPayload schema

--- app/schemas/token.py
+++ app/schemas/token.py
@@ -1,3 +1,4 @@
+from datetime import datetime  # noqa: TC003
 from typing import Literal
 
 from pydantic import BaseModel
@@ -6,3 +7,8 @@ from pydantic import BaseModel
 class Token(BaseModel):
     access_token: str
     token_type: Literal["bearer"] = "bearer"  # noqa: S105
+
+
+class JWTPayload(BaseModel):
+    sub: str
+    exp: datetime | None = None
