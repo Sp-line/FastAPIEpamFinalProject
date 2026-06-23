f928c9bf946875f8950738d958e177c438b7164f
spline <dmcemin@gmail.com>
2026-06-23 16:14:30 +0200

    add: JWTService provider

--- app/dependencies/services.py
+++ app/dependencies/services.py
@@ -2,13 +2,23 @@ from dishka import Provider
 from dishka import Scope
 from dishka import provide
 
+from app.core.auth.jwt import JWTService
 from app.core.auth.password import PasswordService
+from app.core.config import settings
 from app.services.user import UserService
 
 
 class ServiceProvider(Provider):
     scope = Scope.REQUEST
 
-    get_password_service = provide(PasswordService)
+    get_password_service = provide(PasswordService, scope=Scope.APP)
+
+    @provide(scope=Scope.APP)
+    def get_jwt_service(self) -> JWTService:
+        return JWTService(
+            secret=settings.auth.secret,
+            algorithm=settings.auth.algorithm,
+            lifetime_seconds=settings.auth.lifetime_seconds,
+        )
 
     get_user_service = provide(UserService)
