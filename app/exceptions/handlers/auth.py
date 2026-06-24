6c01f718d3b59dad3028d49dd980f3d68b9019bc
spline <dmcemin@gmail.com>
2026-06-24 18:55:38 +0200

    add: TokenExpiredError & TokenInvalidError handlers

--- app/exceptions/handlers/auth.py
+++ app/exceptions/handlers/auth.py
@@ -5,6 +5,8 @@ from fastapi.responses import JSONResponse
 
 from app.constants.messages.auth import AuthErrorMessage
 from app.exceptions.auth import InvalidCredentialsError
+from app.exceptions.auth import TokenExpiredError
+from app.exceptions.auth import TokenInvalidError
 
 
 async def invalid_credentials_handler(
@@ -12,10 +14,28 @@ async def invalid_credentials_handler(
 ) -> JSONResponse:
     return JSONResponse(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        content={"detail": AuthErrorMessage.INVALID_CREDENTIALS.value},
+        content={"detail": AuthErrorMessage.INVALID_CREDENTIALS},
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+
+async def token_expired_handler(_: Request, __: TokenExpiredError) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        content={"detail": AuthErrorMessage.TOKEN_EXPIRED},
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+
+async def token_invalid_handler(_: Request, __: TokenInvalidError) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        content={"detail": AuthErrorMessage.TOKEN_INVALID},
         headers={"WWW-Authenticate": "Bearer"},
     )
 
 
 def register_auth_exception_handlers(app: FastAPI) -> None:
     app.add_exception_handler(InvalidCredentialsError, invalid_credentials_handler)  # type: ignore[arg-type]
+    app.add_exception_handler(TokenExpiredError, token_expired_handler)  # type: ignore[arg-type]
+    app.add_exception_handler(TokenInvalidError, token_invalid_handler)  # type: ignore[arg-type]
