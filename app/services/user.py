81746a1e3f4eeecd86b6dfa2d2d75708570064c3
spline <dmcemin@gmail.com>
2026-06-24 13:32:28 +0200

    use: absolute imports

--- app/services/user.py
+++ app/services/user.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from exceptions.db import ObjectNotFoundError
+from app.exceptions.db import ObjectNotFoundError
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
