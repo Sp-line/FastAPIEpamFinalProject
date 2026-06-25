713908a73817b155b7a3050e996899639e0dad84
spline <dmcemin@gmail.com>
2026-06-25 16:47:28 +0200

    add: delete_project endpoint

--- app/api/api_v1/project.py
+++ app/api/api_v1/project.py
@@ -12,6 +12,7 @@ from app.dependencies.auth import get_current_user_id
 from app.schemas.project import ProjectCreateReq  # noqa: TC001
 from app.schemas.project import ProjectRead  # noqa: TC001
 from app.usages.projects.create import ProjectCreateUsage  # noqa: TC001
+from app.usages.projects.delete import ProjectDeleteUsage  # noqa: TC001
 
 router = APIRouter(route_class=DishkaRoute, prefix=settings.api.v1.projects)
 
@@ -26,3 +27,15 @@ async def create_project(
     current_user_id: Annotated[PositiveInt, Depends(get_current_user_id)],
 ) -> ProjectRead:
     return await project_create_usage(data, current_user_id)
+
+
+@router.delete(
+    "/{project_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_project(
+    project_id: int,
+    project_delete_usage: FromDishka[ProjectDeleteUsage],
+    current_user_id: Annotated[PositiveInt, Depends(get_current_user_id)],
+) -> None:
+    await project_delete_usage(project_id, current_user_id)
