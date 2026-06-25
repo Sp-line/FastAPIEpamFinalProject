c258bc28546b54e80b8e9a798ddd85efa83c3240
spline <dmcemin@gmail.com>
2026-06-25 18:44:02 +0200

    add: PROJECT_UPDATE_FORBIDDEN message

--- app/constants/messages/authorization.py
+++ app/constants/messages/authorization.py
@@ -3,3 +3,4 @@ from enum import StrEnum
 
 class AuthorizationErrorMessage(StrEnum):
     PROJECT_DELETE_FORBIDDEN = "Only project owner can delete the project"
+    PROJECT_UPDATE_FORBIDDEN = "Only project owner or participant can update the project"
