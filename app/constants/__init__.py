977c87048a69b455020f6766db1e2fe0b329f7b6
spline <dmcemin@gmail.com>
2026-06-26 19:28:15 +0200

    add: DocumentMimeType StrEnum import to __all__

--- app/constants/__init__.py
+++ app/constants/__init__.py
@@ -1,6 +1,7 @@
 __all__ = (
     "DBDriver",
     "DocumentLimits",
+    "DocumentMimeType",
     "EnvironmentType",
     "JWTAlgorithm",
     "PostgresErrorCode",
@@ -14,6 +15,7 @@ from app.constants.auth import JWTAlgorithm
 from app.constants.db import DBDriver
 from app.constants.db import PostgresErrorCode
 from app.constants.document import DocumentLimits
+from app.constants.document import DocumentMimeType
 from app.constants.env_type import EnvironmentType
 from app.constants.project import ProjectLimits
 from app.constants.project_member import ProjectMemberLimits
