d5c938742560fcf4386e1bd0f3f15bef7a0ebbad
spline <dmcemin@gmail.com>
2026-06-26 19:27:48 +0200

    add: DocumentMimeType StrEnum

--- app/constants/document.py
+++ app/constants/document.py
@@ -1,6 +1,15 @@
+from enum import StrEnum
+
+
 class DocumentLimits:
     ORIGINAL_NAME_MAX: int = 255
     ORIGINAL_NAME_MIN: int = 1
 
     S3_KEY_MAX: int = 1024
     S3_KEY_MIN: int = 36
+
+
+class DocumentMimeType(StrEnum):
+    PDF = "application/pdf"
+    DOC = "application/msword"
+    DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
