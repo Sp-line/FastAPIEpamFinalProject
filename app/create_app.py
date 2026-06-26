b3468a38dd5358253468fd2591122da51f341bae
spline <dmcemin@gmail.com>
2026-06-26 14:03:13 +0200

    add: disable api docs if env is Production

--- app/create_app.py
+++ app/create_app.py
@@ -2,6 +2,8 @@ from dishka import make_async_container
 from dishka.integrations.fastapi import setup_dishka as setup_fastapi_dishka
 from fastapi import FastAPI
 
+from app.constants.env_type import EnvironmentType
+from app.core.config import settings
 from app.dependencies import InfrastructureProvider
 from app.dependencies import RepositoryProvider
 from app.dependencies import ServiceProvider
@@ -10,7 +12,10 @@ from app.exceptions.handlers import register_exception_handlers
 
 
 def create() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(
+        docs_url="/docs" if settings.env != EnvironmentType.PROD else None,
+        redoc_url="/redoc" if settings.env != EnvironmentType.PROD else None,
+    )
 
     container = make_async_container(
         InfrastructureProvider(),
