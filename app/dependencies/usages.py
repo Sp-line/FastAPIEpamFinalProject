8e0d9b4656ff35e8f1d4adfd457c37adc29568d2
spline <dmcemin@gmail.com>
2026-06-27 15:34:54 +0200

    add: document usages providers

--- app/dependencies/usages.py
+++ app/dependencies/usages.py
@@ -2,6 +2,11 @@ from dishka import Provider
 from dishka import Scope
 from dishka import provide
 
+from app.usages.documents.create import DocumentCreateUsage
+from app.usages.documents.delete import DocumentDeleteUsage
+from app.usages.documents.list import DocumentListUsage
+from app.usages.documents.retrieve import DocumentRetrieveUsage
+from app.usages.documents.update import DocumentUpdateUsage
 from app.usages.projects.create import ProjectCreateUsage
 from app.usages.projects.delete import ProjectDeleteUsage
 from app.usages.projects.list import ProjectListInfoUsage
@@ -20,3 +25,9 @@ class UsagesProvider(Provider):
     get_project_update_usage = provide(ProjectUpdateUsage)
     get_project_retrieve_info_usage = provide(ProjectRetrieveInfoUsage)
     get_project_list_info_usage = provide(ProjectListInfoUsage)
+
+    get_document_create_usage = provide(DocumentCreateUsage)
+    get_document_delete_usage = provide(DocumentDeleteUsage)
+    get_document_retrieve_usage = provide(DocumentRetrieveUsage)
+    get_document_list_usage = provide(DocumentListUsage)
+    get_document_update_usage = provide(DocumentUpdateUsage)
