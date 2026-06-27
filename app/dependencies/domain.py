f927304dafec052909fd276b56f57c64b5126448
spline <dmcemin@gmail.com>
2026-06-27 15:29:15 +0200

    add: EnsureCanDeleteDocument, EnsureCanUpdateDocument, EnsureCanRetrieveDocument, EnsureCanCreateDocument & EnsureCanListDocument providers

--- app/dependencies/domain.py
+++ app/dependencies/domain.py
@@ -2,6 +2,11 @@ from dishka import Provider
 from dishka import Scope
 from dishka import provide
 
+from app.domain.document import EnsureCanCreateDocument
+from app.domain.document import EnsureCanDeleteDocument
+from app.domain.document import EnsureCanListDocument
+from app.domain.document import EnsureCanRetrieveDocument
+from app.domain.document import EnsureCanUpdateDocument
 from app.domain.project import EnsureCanDeleteProject
 from app.domain.project import EnsureCanRetrieveProject
 from app.domain.project import EnsureCanUpdateProject
@@ -13,3 +18,9 @@ class DomainProvider(Provider):
     get_ensure_can_delete_project = provide(EnsureCanDeleteProject)
     get_ensure_can_update_project = provide(EnsureCanUpdateProject)
     get_ensure_can_retrieve_project = provide(EnsureCanRetrieveProject)
+
+    get_ensure_can_delete_document = provide(EnsureCanDeleteDocument)
+    get_ensure_can_update_document = provide(EnsureCanUpdateDocument)
+    get_ensure_can_retrieve_document = provide(EnsureCanRetrieveDocument)
+    get_ensure_can_list_document = provide(EnsureCanListDocument)
+    get_ensure_can_create_document = provide(EnsureCanCreateDocument)
