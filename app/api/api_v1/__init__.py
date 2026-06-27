b55e2c227100a1b0c857b4a21600e7925c546999
spline <dmcemin@gmail.com>
2026-06-27 15:42:07 +0200

    include: document router to api v1 router

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.api_v1.auth import router as auth_router
+from app.api.api_v1.document import router as document_router
 from app.api.api_v1.project import router as project_router
 from app.core.config import settings
 
@@ -10,3 +11,4 @@ router = APIRouter(
 
 router.include_router(auth_router, tags=["User"])
 router.include_router(project_router, tags=["Project"])
+router.include_router(document_router, tags=["Document"])
