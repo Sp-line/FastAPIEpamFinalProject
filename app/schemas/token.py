681b6dacefee15dd2458ec203d47e68f68385792
spline <dmcemin@gmail.com>
2026-07-01 18:13:59 +0200

    add: InviteJWTPayload

--- app/schemas/token.py
+++ app/schemas/token.py
@@ -3,6 +3,9 @@ from typing import Literal
 
 from pydantic import BaseModel
 from pydantic import ConfigDict
+from pydantic import PositiveInt
+
+from app.constants.token import TokenType
 
 
 class Token(BaseModel):
@@ -10,8 +13,21 @@ class Token(BaseModel):
     token_type: Literal["bearer"] = "bearer"  # noqa: S105
 
 
-class JWTPayload(BaseModel):
+class JWTPayloadBase(BaseModel):
     sub: str
     exp: datetime | None = None
+    type: TokenType
+
+
+class JWTPayload(JWTPayloadBase):
+    type: Literal[TokenType.ACCESS] = TokenType.ACCESS
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class InviteJWTPayload(JWTPayloadBase):
+    type: Literal[TokenType.PROJECT_INVITE] = TokenType.PROJECT_INVITE
+
+    project_id: PositiveInt
 
     model_config = ConfigDict(from_attributes=True)
