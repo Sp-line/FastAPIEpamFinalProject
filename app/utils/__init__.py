a0796a68b2ccc38f812beb29365a43a276254d70
spline <dmcemin@gmail.com>
2026-07-02 13:24:48 +0200

    add: UrlBuilder

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ("camel_case_to_snake_case", "validate_password_strength")
+__all__ = ("UrlBuilder", "camel_case_to_snake_case", "validate_password_strength")
 
 from app.utils.case_converter import camel_case_to_snake_case
 from app.utils.password_validator import validate_password_strength
+from app.utils.url import UrlBuilder
